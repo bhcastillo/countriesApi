f87f4c8b2545f3cb10bda9d7940698841a621cfb
Brayan Castillo <bhcastillor@gmail.com>
2023-08-18 14:56:08 -0500

    chore: test

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,40 @@
-import bodyParser               from 'body-parser'
-import express, { Application } from 'express'
-import helmet                   from 'helmet'
-import morgan                   from 'morgan'
-import cors                     from 'cors'
+import bodyParser from "body-parser";
+import express, { Application } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
 
-import { countriesRouter } from './routers/countries'
-import { indexRouter }     from './routers'
-import { config }          from './config'
+import { countriesRouter } from "./routers/countries";
+import { indexRouter } from "./routers";
+import { config } from "./config";
 
 export class App {
-  port: number = config.PORT
+  port: number = config.PORT;
 
-  private readonly app: Application
+  private readonly app: Application;
 
-  constructor () {
-    this.app = express()
-    this.initConfig()
-    this.routes()
+  constructor() {
+    this.app = express();
+    console.log("jaja");
+    this.initConfig();
+    this.routes();
   }
 
-  private initConfig (): void {
-    this.app.use(morgan('dev'))
-    this.app.use(helmet())
-    this.app.use(bodyParser.json())
-    this.app.use(cors())
+  private initConfig(): void {
+    this.app.use(morgan("dev"));
+    this.app.use(helmet());
+    this.app.use(bodyParser.json());
+    this.app.use(cors());
   }
 
-  private routes (): void {
-    this.app.use('', indexRouter)
-    this.app.use('/api', countriesRouter)
+  private routes(): void {
+    this.app.use("", indexRouter);
+    this.app.use("/api", countriesRouter);
   }
 
-  listen (): void {
+  listen(): void {
     this.app.listen(this.port, () => {
-      console.log(`Server running on port ${this.port}`)
-    })
+      console.log(`Server running on port ${this.port}`);
+    });
   }
 }
